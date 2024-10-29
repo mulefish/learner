8cde2f6302feec122cd1ce93ccd238e6fe4f069b
Paul Montgomery <finch.wren.swift@gmail.com>
2024-10-29 10:51:24 -0700

    First pass - learning IDEA's ideas about github.

--- src/test/java/learner/HelloworldTest.java
+++ src/test/java/learner/HelloworldTest.java
@@ -1,7 +1,9 @@
     package learner;
 
-//    import org.junit.jupiter.api.Test;
-//    import static org.junit.jupiter.api.Assertions.assertEquals;
+    import org.junit.jupiter.api.Test;
+    import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
 
     public class HelloworldTest {
 
